a365c440b3c9ce16fa8da89eac8623bcf91f6ae0
Adham <luadham@gmail.com>
2021-03-03 20:16:45 +0200

    Second Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    for (int i = 0; i < 10; i++) {
+	        for (int j = 0; j <= i; j++){
+	            System.out.print("*");
+            }
+	        System.out.println();
+        }
     }
 }
